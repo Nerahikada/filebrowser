4d517765435fec2ba05c635d8476ce3e7052ba74
ねらひかだ <hkshirorom@yahoo.co.jp>
2024-01-26 12:10:56 +0900

    feat: implement jwt header auth method
    Squashed commit of the following:
    
    commit 505cd72389b2d82726cd86c5c28982387a7aff12
    Author: ねらひかだ <hkshirorom@yahoo.co.jp>
    Date:   Fri Jan 26 12:06:51 2024 +0900
    
        chore: fix lint
    
    commit a6cb668883b7d742af956d19bc54804d9e7a6cb5
    Author: ねらひかだ <hkshirorom@yahoo.co.jp>
    Date:   Thu Jan 25 10:23:32 2024 +0900
    
        feat(auth): create a user if the user does not exist
    
    commit 7e448d7b1fc36ff06c55b6684bbc574a73222bf1
    Author: ねらひかだ <hkshirorom@yahoo.co.jp>
    Date:   Wed Jan 24 01:33:56 2024 +0900
    
        fix: change to use default configuration values
    
    commit 916c86899c287e1c4ccf97682c6f262976d23428
    Author: ねらひかだ <hkshirorom@yahoo.co.jp>
    Date:   Tue Jan 23 20:35:39 2024 +0900
    
        feat: Imprement jwt-auth header method
        Continued from #2474
    
        Co-authored-by: Travis Johnson <travis@thisguy.codes>

---
+++ auth/jwt.go
@@ -0,0 +1,129 @@
+package auth
+
+import (
+	"context"
+	"crypto/rand"
+	"encoding/base64"
+	nerrors "errors"
+	"log"
+	"math"
+	"net/http"
+	"os"
+	"sync"
+
+	"github.com/coreos/go-oidc/v3/oidc"
+
+	"github.com/filebrowser/filebrowser/v2/errors"
+	"github.com/filebrowser/filebrowser/v2/settings"
+	"github.com/filebrowser/filebrowser/v2/users"
+)
+
+// MethodJWTAuth is used to identify JWTAuth auth.
+const MethodJWTAuth settings.AuthMethod = "jwt-header"
+
+// JWTAuth is a JWTAuth implementation of an auther.
+type JWTAuth struct {
+	CertsURL      string `json:"certsurl"`
+	Aud           string `json:"aud"`
+	Iss           string `json:"iss"`
+	UsernameClaim string `json:"usernameClaim"`
+	Header        string `json:"header"`
+	remoteKeySet  *oidc.RemoteKeySet
+	init          sync.Once
+}
+
+// Auth authenticates the user via a JWT token in an HTTP header.
+func (a *JWTAuth) Auth(r *http.Request, usr users.Store, stg *settings.Settings, srv *settings.Server) (*users.User, error) {
+	a.init.Do(func() {
+		a.remoteKeySet = oidc.NewRemoteKeySet(context.Background(), a.CertsURL)
+	})
+
+	accessJWT := r.Header.Get(a.Header)
+	if accessJWT == "" {
+		return nil, os.ErrPermission
+	}
+
+	// The Application Audience (AUD) tag for your application
+	config := &oidc.Config{
+		ClientID: a.Aud,
+	}
+
+	verifier := oidc.NewVerifier(a.Iss, a.remoteKeySet, config)
+
+	token, err := verifier.Verify(r.Context(), accessJWT)
+	if err != nil {
+		return nil, os.ErrPermission
+	}
+
+	payload := map[string]any{}
+	err = token.Claims(&payload)
+	if err != nil {
+		return nil, os.ErrPermission
+	}
+
+	user, err := usr.Get(srv.Root, payload[a.UsernameClaim])
+	if nerrors.Is(err, errors.ErrNotExist) {
+		user, err = a.createNewUser(payload[a.UsernameClaim].(string), usr, stg, srv)
+		if err != nil {
+			return nil, err
+		}
+		return user, nil
+	}
+
+	return user, err
+}
+
+// LoginPage tells that proxy auth doesn't require a login page.
+func (a *JWTAuth) LoginPage() bool {
+	return false
+}
+
+func (a *JWTAuth) createNewUser(name string, usr users.Store, stg *settings.Settings, srv *settings.Server) (*users.User, error) {
+	rnd, err := randomBase64String(16) //nolint:gomnd
+	if err != nil {
+		return nil, err
+	}
+	pwd, err := users.HashPwd(rnd)
+	if err != nil {
+		return nil, err
+	}
+
+	user := &users.User{
+		Username:     name,
+		Password:     pwd,
+		LockPassword: true,
+	}
+	stg.Defaults.Apply(user)
+
+	home, err := stg.MakeUserDir(user.Username, user.Scope, srv.Root)
+	if err != nil {
+		return nil, err
+	}
+	user.Scope = home
+
+	err = usr.Save(user)
+	if nerrors.Is(err, errors.ErrExist) {
+		user, err = usr.Get(srv.Root, user.Username)
+		if err != nil {
+			return nil, err
+		}
+		return user, nil
+	} else if err != nil {
+		return nil, err
+	}
+
+	log.Printf("new user: %s, home dir: [%s].", user.Username, user.Scope)
+
+	return user, nil
+}
+
+// see: https://stackoverflow.com/a/55860599
+func randomBase64String(l int) (string, error) {
+	buff := make([]byte, int(math.Ceil(float64(l)*0.75))) //nolint:gomnd
+	_, err := rand.Read(buff)
+	if err != nil {
+		return "", err
+	}
+	str := base64.RawURLEncoding.EncodeToString(buff)
+	return str[:l], nil
+}
